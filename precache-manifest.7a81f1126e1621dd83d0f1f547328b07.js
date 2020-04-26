5372475719bc59bd7149591d36aee3133d35e835
William Harvey <william.harvey@polymtl.ca>
2020-04-26 00:13:55 -0400

    Updates

---
+++ precache-manifest.7a81f1126e1621dd83d0f1f547328b07.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ce674f14e50c0bc281a56706d94c3c",
+    "url": "/openchallenge2020/index.html"
+  },
+  {
+    "revision": "59b0a13655df02ca327c",
+    "url": "/openchallenge2020/static/css/main.8071b14a.chunk.css"
+  },
+  {
+    "revision": "eac10e96243c44b8d9f2",
+    "url": "/openchallenge2020/static/js/2.364ae952.chunk.js"
+  },
+  {
+    "revision": "9aae5077729947dfa90ccff7014f56af",
+    "url": "/openchallenge2020/static/js/2.364ae952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b0a13655df02ca327c",
+    "url": "/openchallenge2020/static/js/main.00a64848.chunk.js"
+  },
+  {
+    "revision": "40564171f5c556f9463d",
+    "url": "/openchallenge2020/static/js/runtime-main.8adfbdfa.js"
+  },
+  {
+    "revision": "fda414f11bc60efd4c9a12c1dcdecd97",
+    "url": "/openchallenge2020/static/media/aep.fda414f1.png"
+  },
+  {
+    "revision": "e3ba6abc64ead779a4210d241889bd11",
+    "url": "/openchallenge2020/static/media/diptrace.e3ba6abc.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/openchallenge2020/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/openchallenge2020/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/openchallenge2020/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/openchallenge2020/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/openchallenge2020/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b8cc6e10e8a749be56a643cd8b073971",
+    "url": "/openchallenge2020/static/media/hypnogram-electrophysiologist.b8cc6e10.csv"
+  },
+  {
+    "revision": "53f2765d19d90baf4ae9719899d1331e",
+    "url": "/openchallenge2020/static/media/hypnogram-openbci-electrophysiologist.53f2765d.csv"
+  },
+  {
+    "revision": "bf6d0c34cfd30695e0b39359ef0d6639",
+    "url": "/openchallenge2020/static/media/hypnogram-openbci-predicted.bf6d0c34.csv"
+  },
+  {
+    "revision": "172957b9050959834d387540d8536477",
+    "url": "/openchallenge2020/static/media/hypnogram-predicted.172957b9.csv"
+  },
+  {
+    "revision": "3c793400626a7b368c486df82d16a805",
+    "url": "/openchallenge2020/static/media/hypnogram.3c793400.csv"
+  },
+  {
+    "revision": "bcea7572194d7b7245345be1e616b877",
+    "url": "/openchallenge2020/static/media/logo.bcea7572.png"
+  },
+  {
+    "revision": "bdd09ab5b6a9db96973a5dec7b18b258",
+    "url": "/openchallenge2020/static/media/ntx.bdd09ab5.png"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/openchallenge2020/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/openchallenge2020/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/openchallenge2020/static/media/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/openchallenge2020/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/openchallenge2020/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "3034d9919e43a4a2ab115b5ade62ab02",
+    "url": "/openchallenge2020/static/media/polymtl.3034d991.png"
+  },
+  {
+    "revision": "53bfde7ac5b2c91c549fd72425eb7957",
+    "url": "/openchallenge2020/static/media/protocase.53bfde7a.png"
+  }
+]);
